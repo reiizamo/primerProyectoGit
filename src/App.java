170c65138b10a4ca5dcfee6f7e239f5d80fe24e8
Asier Fernández <asierferzam@gmail.com>
2026-03-10 13:27:10 +0000

    mensaje con los cambios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import net.salesianos.persona.Persona;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("hola clase de 1º DAM");
+
+        Persona p1 = new Persona("Juan", 25, "Pérez");
+
     }
 }
---
+++ src/net/salesianos/persona/Persona.java
@@ -0,0 +1,13 @@
+package net.salesianos.persona;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String apellido;
+
+    public Persona(String nombre, int edad, String apellido) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apellido = apellido;
+    }
+}
